455e7b8492e83519f6c944e4dfbb5b59d85dc2e7
Mona Omeragic <mona.omeragic@stu.ibu.edu.ba>
2025-06-08 19:13:37 +0200

    project

--- frontend/utils/constants.js
+++ frontend/utils/constants.js
@@ -3,7 +3,7 @@ window.Constants = {
   PROJECT_BASE_URL:
     window.location.hostname === "localhost" || window.location.hostname === "127.0.0.1"
       ? "http://localhost/MonaOmeragic/Web-programming/backend"
-      : "https://whale-app-2-rytvu.ondigitalocean.app/",
+      : "https://whale-app-2-rytvu.ondigitalocean.app",
   USER_ROLE: "student",
   ADMIN_ROLE: "admin",
   PROFESSOR_ROLE: "professor",
